6e136683ce9831a6867581b4b8258e5202cc30cb
Haise2312 <103759254+Haise2312@users.noreply.github.com>
2023-03-25 16:30:33 +0700

    up

--- app/src/main/java/com/example/appfoodv2/View/Admin/HomeAdminActivity.java
+++ app/src/main/java/com/example/appfoodv2/View/Admin/HomeAdminActivity.java
@@ -3,39 +3,55 @@ package com.example.appfoodv2.View.Admin;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageView;
+
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import com.example.appfoodv2.R;
 
-public class HomeAdminActivity  extends AppCompatActivity implements View.OnClickListener {
-    private ImageView imgdangsanpham;
+public class HomeAdminActivity  extends AppCompatActivity {
+    CardView cvHoaDon, cvThongKe, cvSignOut, cvSanpham;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_admin);
-        findViewById(R.id.cHoaDon).setOnClickListener(this);
-        findViewById(R.id.cThongKe).setOnClickListener(this);
-        findViewById(R.id.cSignOut).setOnClickListener(this);
-        findViewById(R.id.imgdangsanpham).setOnClickListener(this);
+
+        cvHoaDon = findViewById(R.id.cvHoaDon);
+        cvThongKe = findViewById(R.id.cvThongKe);
+        cvSignOut = findViewById(R.id.cvSignOut);
+        cvSanpham =findViewById(R.id.cvSanpham);
 
 
+        cvHoaDon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeAdminActivity.this, BillADMiNActivity.class);
+                startActivity(intent);
+            }
+        });
+        cvThongKe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeAdminActivity.this, ChartBillActivity.class);
+                startActivity(intent);
+            }
+        });
+        cvSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeAdminActivity.this, SignInAdminActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        cvSanpham.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeAdminActivity.this, ProductActivity.class);
+                startActivity(intent);
+            }
+        });
     }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.cHoaDon: startActivity(new Intent( HomeAdminActivity.this,BillADMiNActivity.class));break;
-
-            case R.id.cThongKe: startActivity(new Intent( HomeAdminActivity.this,ChartBillActivity.class));break;
-            case R.id.cSignOut: finish();break;
-            case R.id.imgdangsanpham: startActivity(new Intent( HomeAdminActivity.this, ProductActivity.class));break;
-
-
-
-        }
-    }
-
 }
