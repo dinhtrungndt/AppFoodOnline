0c3527db8e33d9d61023e6d40762c394e8e7d535
dinhtrung1605 <nguyendinhtrung.it@gmail.com>
2023-03-29 13:46:57 +0700

    Up-Mess

--- app/src/main/java/com/example/appfoodv2/View/FragMent/FragMent_Message.java
+++ app/src/main/java/com/example/appfoodv2/View/FragMent/FragMent_Message.java
@@ -1,14 +1,13 @@
 package com.example.appfoodv2.View.FragMent;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 import com.example.appfoodv2.R;
 
@@ -16,8 +15,8 @@ public class FragMent_Message extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_message,container,false);
+        view = inflater.inflate(R.layout.fragment_home, container, false);
 
-        return view;
+        return ;
     }
 }
